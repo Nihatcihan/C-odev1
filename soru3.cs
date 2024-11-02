b1b5e432eb8e2a15ad2faf5e548a2bbdf00e8265
Nihat Cihan <nihat.cihan7@gmail.com>
2024-11-03 01:17:19 +0300

    soru3 cozuldu

---
+++ soru3.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). Sonrasında kullanıcıdan n adet kelime girmesi isteyin. Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
+Console.WriteLine("pozitif bir sayi giriniz");
+int n=Convert.ToInt16(Console.ReadLine());
+string[] dizi = new string[n];
+Console.WriteLine(n+" adet kelime giriniz");
+
+for (int i = 0; i < n; i++)
+   dizi[i]= Console.ReadLine();
+
+for (int i = n; i > 0; i--)
+   Console.WriteLine(dizi[i-1]);
